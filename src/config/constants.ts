14213140c848bce2bd70ddc96e4f3acc28229955
toni <87004940+atortellini@users.noreply.github.com>
2025-09-01 19:51:15 -0700

    wip constants

---
+++ src/config/constants.ts
@@ -0,0 +1,14 @@
+export const APP_NAME;
+export const APP_VERSION;
+
+export const DEFAULT_POLLING_INTERVAL_MS = 60000;
+export const MAX_STREAMERS_PER_REQUEST = 100;
+export const API_TIMEOUT_MS = 10000;
+export const RECONNECT_DELAY_MS = 5000;
+
+export enum LOG_LEVELS {
+    ERROR,
+    WARN,
+    INFO,
+    DEBUG
+}
