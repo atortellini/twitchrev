db02a2d93eb68c9ef5427b888a99c10c7efe2982
toni <87004940+atortellini@users.noreply.github.com>
2025-09-01 19:56:21 -0700

    Unsure of interface for live status manager

--- src/domain/interfaces/IStreamersLiveStatusManager.ts
+++ src/domain/interfaces/IStreamersLiveStatusManager.ts
@@ -1,9 +1,15 @@
 import { Streamer, Platform } from '../models';
 
 export interface IStreamersLiveStatusManager {
-    addStreamer(name: string, platform: Platform): Promise<void>;
-    removeStreamer(name: string, platform: Platform): Promise<boolean>;
-    getTrackedStreamers(): Promise<Streamer[]>;
-    getStreamersByPlatform(platform: Platform): Promise<Streamer[]>;
-    isStreamerTracked(name: string, platform: Platform): Promise<boolean>;
+    // addStreamer(name: string, platform: Platform): Promise<void>;
+    // removeStreamer(name: string, platform: Platform): Promise<boolean>;
+    // getTrackedStreamers(): Promise<Streamer[]>;
+    // getStreamersByPlatform(platform: Platform): Promise<Streamer[]>;
+    // isStreamerTracked(name: string, platform: Platform): Promise<boolean>;
+    /**
+     * TODO:
+     *  Not sure about this interface and also whether I should have the interface require
+     * Streamer objs to interact with them. Downsides are that those interacting with the
+     * interfaces have to have access to the platform specific api
+     */
 }
