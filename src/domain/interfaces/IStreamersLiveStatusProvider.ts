6afc2eda881c9418c04646dd515ca89438ce4279
toni <87004940+atortellini@users.noreply.github.com>
2025-09-01 21:32:26 -0700

    Changed IPlatformStreamerLiveTracker methods to simply work with strings (usernames of streamers). Changed IStreamersLiveStatusProvider to take callbacks which take StreamStatus as param instead of just Streamer. Provides more info to callbacks with StreamStatus as argument.

--- src/domain/interfaces/IStreamersLiveStatusProvider.ts
+++ src/domain/interfaces/IStreamersLiveStatusProvider.ts
@@ -1,8 +1,8 @@
 import { StreamStatus, Streamer } from '../models';
 
 export interface IStreamersLiveStatusProvider {
-    onStreamerWentLive(callback: (streamer: Streamer) => void): void;
-    onStreamerWentOffline(callback: (streamer: Streamer) => void): void;
+    onStreamerWentLive(callback: (streamer: StreamStatus) => void): void;
+    onStreamerWentOffline(callback: (streamer: StreamStatus) => void): void;
     onStreamerAdded(callback: (streamer: Streamer) => void): void;
     onStreamerRemoved(callback: (streamer: Streamer) => void): void;
 }
