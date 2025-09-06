c683482209fb872e568a5df65833c67e7778a24b
toni <87004940+atortellini@users.noreply.github.com>
2025-09-06 15:45:56 -0700

    Live tracker interfaces changed to require Streamer objects as arguments rather than just strings.

--- src/domain/interfaces/IStreamersLiveStatusManager.ts
+++ src/domain/interfaces/IStreamersLiveStatusManager.ts
@@ -1,15 +1,16 @@
-import { Streamer, Platform } from '../models';
+import {Platform, Streamer} from '../models';
 
 export interface IStreamersLiveStatusManager {
-    // addStreamer(name: string, platform: Platform): Promise<void>;
-    // removeStreamer(name: string, platform: Platform): Promise<boolean>;
-    // getTrackedStreamers(): Promise<Streamer[]>;
-    // getStreamersByPlatform(platform: Platform): Promise<Streamer[]>;
-    // isStreamerTracked(name: string, platform: Platform): Promise<boolean>;
-    /**
-     * TODO:
-     *  Not sure about this interface and also whether I should have the interface require
-     * Streamer objs to interact with them. Downsides are that those interacting with the
-     * interfaces have to have access to the platform specific api
-     */
+  addStreamer(name: Streamer, platform: Platform): Promise<void>;
+  removeStreamer(name: Streamer, platform: Platform): Promise<boolean>;
+  getTrackedStreamers(): Promise<Streamer[]>;
+  getStreamersByPlatform(platform: Platform): Promise<Streamer[]>;
+  isStreamerTracked(name: Streamer, platform: Platform): Promise<boolean>;
+  /**
+   * TODO:
+   *  Not sure about this interface and also whether I should have the interface
+   * require Streamer objs to interact with them. Downsides are that those
+   * interacting with the interfaces have to have access to the platform
+   * specific api
+   */
 }
