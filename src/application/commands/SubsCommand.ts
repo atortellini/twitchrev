4c4570ff88670cf41c2788e92c1aef25744c6ab5
toni <87004940+atortellini@users.noreply.github.com>
2025-09-14 21:58:19 -0700

    A couple basic chatbot commands (#11)
    * Simple creator middleware to only allow a particular user (me) to execute particular commands of the chatbot.
    
    * Track command implementation. Allows for control over the set of streamers being live/subevent tracked through using the manager interfaces.
    
    * Subs command implementation. Queries the livestream metrics provider for the subscription info it posesses for the particular streamer. For now, simply returns the total number of subs.

---
+++ src/application/commands/SubsCommand.ts
@@ -0,0 +1,62 @@
+import {IBotCommand, IPlatformUsersAPI} from '../../domain/interfaces';
+import {Platform, SUPPORTED_PLATFORMS, TwitchChatCommand} from '../../domain/models';
+import {CreatorMiddleware} from '../middleware/CreatorMiddleware';
+import {ILiveStreamSessionMetricsProvider} from '../services/LiveStreamsSessionMetricsCoordinator';
+
+export class SubsCommand implements IBotCommand {
+  readonly trigger = 'subs';
+  readonly description = 'Query the number of subs earned for a live streamer';
+  readonly usage = '$subs <username> [<platform=twitch>]';
+  readonly middleware = CreatorMiddleware;
+
+  constructor(
+      private readonly stream_metrics_provider:
+          ILiveStreamSessionMetricsProvider<Platform>,
+      private readonly twitch_api: IPlatformUsersAPI<Platform.Twitch>) {}
+
+  canExecute(command: TwitchChatCommand): boolean {
+    return (
+        (this.trigger === command.trigger) &&
+        this.middleware.canExecute(command));
+  }
+
+  async execute(command: TwitchChatCommand): Promise<string> {
+    const args_str = command.rest;
+
+    if (!args_str) {
+      return `Usage: ${this.usage}`;
+    }
+
+    const args = args_str.split(' ');
+
+    const validNumArgs = ((args.length === 1) || (args.length === 2));
+
+    if (!validNumArgs) {
+      return `Usage: ${this.usage}`;
+    }
+
+    const [username, platform_str] = args;
+
+    const platform = platform_str?.toLowerCase() as Platform || Platform.Twitch;
+
+
+    if (!SUPPORTED_PLATFORMS.includes(platform)) {
+      return `Unsupported platform. Supported platforms: ${
+          SUPPORTED_PLATFORMS.join(', ')}`;
+    }
+
+    const user = await this.twitch_api.getUser(username);
+    if (!user) {
+      return `Unknown user ${username} on ${platform}`;
+    }
+
+    const metrics = this.stream_metrics_provider.getSessionMetrics(user);
+
+    if (!metrics) {
+      return `Metrics are not being tracked for ${user.name} on ${
+          user.platform}`;
+    }
+
+    return `${user.name}: ${metrics.subscriptions.totals.all} sub(s) total`;
+  }
+}
